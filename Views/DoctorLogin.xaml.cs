f0e27809df5908cfa9d4d326c56e744841e8f6f7
Jacek <jacek.kurzydlo@protonmail.ch>
2024-03-08 17:41:35 +0100

    Dodaj pliki projektów.

---
+++ Views/DoctorLogin.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace bazy1.Views
+{
+	public partial class DoctorLogin : UserControl
+	{
+		public DoctorLogin()
+		{
+			InitializeComponent();
+		}
+
+		private void BtnMinimizeClick(object sender, RoutedEventArgs e)
+		{
+			Window.GetWindow(this).WindowState = WindowState.Minimized;
+		}
+
+		private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.LeftButton.Equals(MouseButtonState.Pressed)) Window.GetWindow(this).DragMove();
+		}
+
+		private void BtnLoginClick(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+
+		private void BtnCloseClick(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+
+	}
+
+}
---
+++ Views/startPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace bazy1.Views
+{
+    /// <summary>
+    /// Logika interakcji dla klasy UserControl1.xaml
+    /// </summary>
+    public partial class startPage : UserControl
+    {
+        public startPage()
+        {
+			InitializeComponent();
+        }
+		private void BtnMinimizeClick(object sender, RoutedEventArgs e)
+		{
+			Application.Current.MainWindow.WindowState = WindowState.Minimized;
+		}
+
+		private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			//if (e.LeftButton.Equals(MouseButtonState.Pressed))
+		}
+
+		private void BtnLoginClick(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+
+		private void BtnCloseClick(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+
+		private void BtnDoctorClick(object sender, RoutedEventArgs e)
+		{
+			//redite
+		}
+	}
+}
