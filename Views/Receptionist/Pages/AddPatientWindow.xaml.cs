e80798689711f4738b9065b7b021e1a9c59e4524
jkurzydlo <87187308+jkurzydlo@users.noreply.github.com>
2024-05-10 23:59:34 +0200

    Naprawiono błędy

---
+++ Views/Receptionist/Pages/AddPatientWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using bazy1.ViewModels.Receptionist.Pages;
+using System.Windows;
+
+namespace bazy1.Views.Receptionist.Pages
+{
+    public partial class AddPatientWindow : Window
+    {
+        public AddPatientWindow()
+        {
+			//InitializeComponent(); ;
+           // DataContext = new AddPatientViewModel();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Tutaj mozesz dodac logikę zwiazana z obsluga zdarzenia dodawania pacjenta,
+            // ale w przypadku uzycia wzorca MVVM, zalecane jest wywołanie komendy z ViewModelu
+            // np. ((AddPatientViewModel)DataContext).AddPatientCommand.Execute(null);
+        }
+
+        private void PhoneNumberTextBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            // Mozesz dodac logikę weryfikacji wprowadzanych danych,
+            // na przyklad, czy wprowadzane znaki są cyframi itp.
+        }
+    }
+}
